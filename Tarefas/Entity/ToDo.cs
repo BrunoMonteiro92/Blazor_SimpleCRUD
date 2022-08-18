b93d9120995eac7bddb83b83f50644aa3f356ad7
Bruno Marques Monteiro <bruno.monteiro@venturus.org.br>
2022-08-18 11:12:46 -0300

    Componentização Blazor

---
+++ Tarefas/Entity/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarefas.Entity
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string? Description { get; set; }
+        public bool Finished { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
